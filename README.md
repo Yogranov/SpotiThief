17fe82b21ac0bfd6ca470b3874dd3c4d767e76cc
Yogranov <yogranov@gmail.com>
2023-02-10 22:21:46 +0200

    Add sync function
    Signed-off-by: Yogranov <yogranov@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # To Be Continued...
+
+Simple example:
+```python
+from SpotiThief import SpotiThief
+
+st = SpotiThief()
+st.sync("https://open.spotify.com/playlist/[PLAYLIST_ID]")
+```
--- SpotiThief.py
+++ SpotiThief.py
@@ -16,10 +16,26 @@ class SpotiThief:
         self.song_list: list = []
 
 
-    def load_playlist_from_spotify(self, spotify_playlist_url: str, save_to_file: bool = False, file_name: str = None):
+    def sync(self, spotify_playlist_url: str = None, media_location: str = DEFAULT_DOWNLOAD_PATH):
+        self.load_playlist_from_spotify(spotify_playlist_url=spotify_playlist_url, should_cache_playlist=False)
+        self.add_youtube_url_to_playlist()
+        self.__cache_playlist()
+
+        songs_to_remove = self.__get_songs_to_remove(media_location)
+        self.__delete_songs(songs_to_remove, media_location)
+
+        self.export_playlist_to_mp3()
+
+
+
+    def load_playlist_from_spotify(self, spotify_playlist_url: str, should_cache_playlist: bool = True):
+        MAX_LOOP_COUNT = 1000
+
         if spotify_playlist_url is None:
             print("No url provided")
             return
+
+        print("Loading playlist from spotify url")
 
         driver = self.__get_driver_instance()
         driver.get(spotify_playlist_url)
@@ -27,7 +43,8 @@ class SpotiThief:
 
         tracks = []
         last_track = None
-        while True:
+
+        for _ in range(MAX_LOOP_COUNT):
             tracks = driver.find_elements(By.XPATH, '//*[@data-testid="tracklist-row"]')
             if last_track == tracks[-1]:
                 break
@@ -38,28 +55,24 @@ class SpotiThief:
             driver.execute_script("arguments[0].scrollIntoView();", last_track)
             sleep(1)
 
-        if save_to_file:
-            if file_name == None:
-                file_name = DEFAULT_PLAYLIST_PATH
-
-            with open(file_name, "w", encoding="utf-8") as f:
-                json.dump(self.song_list, f, indent=4, ensure_ascii=False)
+        if should_cache_playlist:
+            self.__cache_playlist()
 
 
-    def save_playlist_to_file(self, file_name: str = None):
-        if self.song_list is None:
-            print("No songs loaded")
+    def save_cached_playlist_to_file(self, file_name: str = None):
+        if self.song_list is None or file_name is None:
+            print("No songs loaded or no file name provided")
             return
         
-        if file_name is None:
-            file_name = DEFAULT_PLAYLIST_PATH
-
-        with open(file_name, "w", encoding="utf-8") as f:
-            json.dump(self.song_list, f, indent=4, ensure_ascii=False)
+        print("Saving cached playlist to file")
+        self.__cache_playlist(filename=file_name)
 
     
     def add_youtube_url_to_playlist(self):
         NUM_OF_DRIVERS_AND_THREADS = 5
+
+        print ("Adding youtube urls to playlist")
+        self.__import_youtube_urls_from_cache()
 
         drivers = []
         for i in range(NUM_OF_DRIVERS_AND_THREADS):
@@ -67,6 +80,9 @@ class SpotiThief:
 
         threads = []
         for i in range(len(self.song_list)):
+            if self.song_list[i].get('youtube_url') is not None:
+                continue
+            
             thread = threading.Thread(target=self.__add_youtube_url_to_song, args=(i, drivers[i % NUM_OF_DRIVERS_AND_THREADS]))
             thread.start()
             threads.append(thread)
@@ -81,27 +97,35 @@ class SpotiThief:
 
 
     def export_playlist_to_mp3(self, location: str = DEFAULT_DOWNLOAD_PATH):
-        MAX_THREADS = 100
+        MAX_THREADS = 15
 
         if self.song_list is None:
             print("No songs loaded")
             return
 
+        print("Exporting playlist to mp3")
+
         threads = []
         for song in self.song_list:
+            song_name = self.__get_full_song_name(song)
+
             if song.get('youtube_url') is None:
                 print(f"Song {song['song_name']} has no youtube url")
                 continue
             
-            if len(threads) > MAX_THREADS:
+            if os.path.isfile(f"{song_name}.mp3"):
+                print(f"Song {song_name} already exists")
+                continue
+
+            if len(threads) >= MAX_THREADS:
                 for thread in threads:
                     thread.join()
                 threads = []
 
-            song_name = self.__get_full_song_name(song)
             thread = threading.Thread(target=self.__download_song, args=(song['youtube_url'], song_name, location))
             thread.start()
             threads.append(thread)
+            sleep(2)
 
         for thread in threads:
             thread.join()
@@ -116,6 +140,7 @@ class SpotiThief:
             print("File does not exist")
             return
             
+        print("Loading playlist from file")
         with open(file_name, "r", encoding="utf-8") as f:
             self.song_list = json.load(f)
 
@@ -179,15 +204,14 @@ class SpotiThief:
             'format':'bestaudio/best',
             'keepvideo':False,
             'outtmpl': f'{location}/{filename}.mp3',
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
         }
 
         with youtube_dl.YoutubeDL(options) as ydl:
             try:
-                ydl.cache.remove()
                 ydl.download([video_info['webpage_url']])
 
             except: # second chance, yes I know this is bad
-                ydl.cache.remove()
                 ydl.download([video_info['webpage_url']]) 
 
         print("Download complete... {}".format(filename))
@@ -223,10 +247,16 @@ class SpotiThief:
         options.add_argument("--disable-extensions")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_argument("--log-level=3")
+        options.add_argument("--show-capture=no")
 
         return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-    def __get_missing_songs(self, location: str = DEFAULT_DOWNLOAD_PATH):
+    def __get_new_songs(self, location: str = DEFAULT_DOWNLOAD_PATH):
+        if not os.path.isdir(location):
+            return []
+
         songs_on_disk = []
         for song in os.listdir(location):
             songs_on_disk.append(song.split(".mp3")[0])
@@ -242,6 +272,9 @@ class SpotiThief:
         return new_songs
 
     def __get_songs_to_remove(self, location: str = DEFAULT_DOWNLOAD_PATH):
+        if not os.path.isdir(location):
+            return []
+
         songs_on_disk = []
         for song in os.listdir(location):
             songs_on_disk.append(song.split(".mp3")[0])
@@ -258,3 +291,37 @@ class SpotiThief:
 
     def __get_full_song_name(self, song):
         return song['song_name'] + " - " + song['artist']
+
+    def __import_youtube_urls_from_cache(self):
+        if not os.path.exists(DEFAULT_PLAYLIST_PATH):
+            return
+
+        old_playlist = []
+        with open(DEFAULT_PLAYLIST_PATH, "r", encoding="utf-8") as f:
+            old_playlist = json.load(f)
+
+        for song in self.song_list:
+            if song.get('youtube_url') is not None:
+                continue
+
+            for old_song in old_playlist:
+                if old_song.get('youtube_url') is None:
+                    continue
+
+                if song['song_name'] == old_song['song_name'] and song['artist'] == old_song['artist']:
+                    song['youtube_url'] = old_song['youtube_url']
+                    break
+
+    def __delete_songs(self, songs_to_delete: list = None, location: str = DEFAULT_DOWNLOAD_PATH):
+        if songs_to_delete is None:
+            print("No songs to delete")
+            return
+
+        print(f"Deleting {len(songs_to_delete)} songs")
+        for song in songs_to_delete:
+            print(f"Deleting song: {song}")
+            os.remove(f"{location}/{song}")
+
+    def __cache_playlist(self, filename: str = DEFAULT_PLAYLIST_PATH):
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(self.song_list, f, indent=4, ensure_ascii=False)
